b52519d8d989d7ff158b9f1d8fa75f8d6fdc07f6
Seyed <abbasi.3195@gmail.com>
2026-07-11 00:23:38 +0330

    Insert into dbo.Customer

---
+++ Scripts/InsertCustomer.sql
@@ -0,0 +1,9 @@
+﻿BULK INSERT dbo.Customer
+FROM 'C:\Alireza\Learning\DB\customers.csv'
+WITH
+(
+	FORMAT = 'CSV',
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	ROWTERMINATOR = '0x0a'
+);
---
+++ dbo/Tables/Customer.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE dbo.Customer (
+    customerId             NVARCHAR (50) NOT NULL,
+    customerName           NVARCHAR (50) NOT NULL,
+    customerType           NVARCHAR (50) NOT NULL,
+    creditTermsDays        TINYINT       NOT NULL,
+    primaryFreightType     NVARCHAR (50) NOT NULL,
+    accountStatus          NVARCHAR (50) NOT NULL,
+    contractStartDate      DATE          NOT NULL,
+    annualRevenuePotential INT           NOT NULL,
+    CONSTRAINT PK_Customer PRIMARY KEY CLUSTERED (customerId ASC)
+);
