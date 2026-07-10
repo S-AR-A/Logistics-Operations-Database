6be7d1246b934bae6ef02bd75a51fd2811b3e6b6
Seyed <abbasi.3195@gmail.com>
2026-07-10 21:57:29 +0330

    Import Driver-Monthly-Metrics.csv

--- Scripts/InsertDriverMonthlyMetric.sql
+++ Scripts/InsertDriverMonthlyMetric.sql
@@ -1,1 +1,10 @@
-﻿
+﻿BULK INSERT dbo.DriverMonthlyMetric
+FROM 'C:\Alireza\Learning\DB\driver_monthly_metrics.csv'
+WITH
+(
+ROWTERMINATOR = '0x0a',
+FIELDTERMINATOR = ',', 
+FIRSTROW = 2
+);
+
+
---
+++ dbo/DriverMonthlyMetric.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE DriverMonthlyMetric
+(
+    driverId                   NVARCHAR(50) NOT NULL ,
+    month                      DATE NOT NULL,
+    tripsCompleted             TINYINT,
+    totalMiles                 INT,
+    totalRevenue               FLOAT(53),
+    averageMpg                 FLOAT(53),
+    totalFuelGallons           FLOAT(53),
+    onTimeDeliveryRate         FLOAT(53),
+    averageIdleHours           FLOAT(53), 
+    PRIMARY KEY (month,driverId)
+)
