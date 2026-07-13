d50cdfcdac7088fe080a03fe4b5674bae115c003
Seyed <abbasi.3195@gmail.com>
2026-07-13 11:31:09 +0330

    Create InsertTrip SP

---
+++ Progammability/Stored Procedures/InsertTrip.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[InsertTrip]
+	@loadId NVARCHAR(50),
+	@dispatchDate DATE
+AS
+	IF NOT EXISTS (SELECT 1 FROM Load WHERE loadId = @loadId)
+		THROW 50000,'No such LoadiD was found, Sire!',0
+	INSERT INTO Trip(tripId,loadId,dispatchDate)
+	values
+		('TRIP'+RIGHT('00000000' + Cast(NEXT VALUE FOR dbo.TripSeq as NVARCHAR(8)),8),@loadId, @dispatchDate)
+RETURN 0
