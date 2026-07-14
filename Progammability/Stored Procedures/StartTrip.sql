82235d1e43ad5a990bdb087e0ed839108233f3b9
Seyed <abbasi.3195@gmail.com>
2026-07-14 06:27:18 +0330

    Create StartTrip SP

---
+++ Progammability/Stored Procedures/StartTrip.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[StartTrip]
+	@TripId NVARCHAR(50)
+AS
+BEGIN
+	IF NOT EXISTS (
+	SELECT 1 FROM Trip 
+	WHERE tripId = @TripId 
+		AND tripStatus = 'Not Started'
+		AND driverId IS NOT NULL
+		AND truckId IS NOT NULL
+	)
+		THROW 50001,
+			'No such trip with ''Not Started'' state or
+			driver is not assigned or truck is not assigned, sire!',
+			1
+	UPDATE Trip
+	SET tripStatus = 'Started'
+	WHERE tripId =@TripId
+END
+RETURN 0
