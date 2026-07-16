c76332bb8e806575b2a803c060a693f4d62bb8a8
Seyed <abbasi.3195@gmail.com>
2026-07-16 04:30:46 +0330

    Create ActiveTrips view

---
+++ vw_ActiveTrips.sql
@@ -0,0 +1,21 @@
+﻿CREATE VIEW [dbo].[vw_ActiveTrips]
+	AS 
+	SELECT 
+		t.truckId,
+		d.firstName + ' ' + d.lastName as driverName,
+		tk.unitNumber,
+		c.customerName,
+		r.originCity + ', ' + r.originState as origin,
+		r.destinationCity + ', ' + r.destinationState as destination,
+		t.tripStatus
+	FROM Trip as t
+	join Driver as d
+		on t.driverId = d.driverId
+	join Truck as tk
+		on t.truckId = tk.truckId
+	join Load as l
+		on t.loadId = l.loadId
+	join Customer as c
+		on l.customerId = c.customerId
+	join Route as r
+		on l.routeId = r.routeId
