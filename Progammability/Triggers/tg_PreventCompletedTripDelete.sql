f2e45babca19a1d731f28a1a7560e1ff3e238346
Seyed <abbasi.3195@gmail.com>
2026-07-17 18:03:38 +0330

    Create tg_PreventCompletedTripDelete trigger

---
+++ Progammability/Triggers/tg_PreventCompletedTripDelete.sql
@@ -0,0 +1,21 @@
+﻿CREATE TRIGGER [tg_PreventCompletedTripDelete]
+ON [dbo].[Trip]
+INSTEAD OF DELETE
+AS
+BEGIN
+	IF EXISTS 
+		(
+		SELECT 1
+		FROM deleted
+		WHERE tripStatus = 'Completed'
+		)
+		THROW 50001,'Cannot delete completed trips',1
+
+		DELETE 
+		FROM Trip
+		WHERE tripId IN
+			(
+				SELECT tripId
+				FROM deleted
+			)
+END
