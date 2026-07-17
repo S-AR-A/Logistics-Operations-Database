cccb6150391106fa70d05dbfd98d23d0a9df3d53
Seyed <abbasi.3195@gmail.com>
2026-07-17 23:38:09 +0330

    Add output to InsertTrip SP

--- Progammability/Stored Procedures/InsertTrip.sql
+++ Progammability/Stored Procedures/InsertTrip.sql
@@ -1,10 +1,16 @@
 ﻿CREATE PROCEDURE [dbo].[InsertTrip]
 	@loadId NVARCHAR(50),
-	@dispatchDate DATE
+	@dispatchDate DATE,
+	@NewTripId NVARCHAR(50) OUTPUT
 AS
+	
 	IF NOT EXISTS (SELECT 1 FROM Load WHERE loadId = @loadId)
-		THROW 50000,'No such LoadiD was found, Sire!',0
+		THROW 50000,'No such Loadid was found, Sire!',1
+	DECLARE @TripId NVARCHAR(50) = 
+		'TRIP'+RIGHT('00000000' + Cast(NEXT VALUE FOR dbo.TripSeq as NVARCHAR(8)),8)
 	INSERT INTO Trip(tripId,loadId,dispatchDate)
 	values
-		('TRIP'+RIGHT('00000000' + Cast(NEXT VALUE FOR dbo.TripSeq as NVARCHAR(8)),8),@loadId, @dispatchDate)
-RETURN 0
+		(@TripId,@loadId, @dispatchDate)
+	
+	SET @NewTripId = @TripId;
+	
