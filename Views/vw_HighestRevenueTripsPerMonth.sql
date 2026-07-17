622b4d4deb1ed99e805f347c3681d64e3152ec6b
Seyed <abbasi.3195@gmail.com>
2026-07-17 18:47:38 +0330

    Create HighestRevenueTripsPerMonth view

---
+++ Views/vw_HighestRevenueTripsPerMonth.sql
@@ -0,0 +1,31 @@
+﻿CREATE VIEW [dbo].[vw_HighestRevenueTripsPerMonth]
+	AS 
+		WITH rankedTable
+		AS
+		(
+		SELECT 		
+				t.tripId,
+				l.revenue,
+				de.actualDatetime,
+				DENSE_RANK() OVER (
+									PARTITION BY 
+										YEAR(de.actualDatetime),
+										MONTH(de.actualDatetime)
+									ORDER BY l.revenue DESC
+								  ) as [rank]
+		FROM dbo.Trip as t
+		JOIN dbo.Load as l
+			ON t.loadId = l.loadId
+		JOIN dbo.DeliveryEvent as de
+			ON t.tripId = de.tripId
+		WHERE 
+			tripStatus = 'Completed'
+			AND de.eventType = 'Delivery'
+		)
+		SELECT 
+			tripId,
+			revenue,
+			actualDatetime,
+			[rank]
+		FROM rankedTable
+		WHERE [rank] <= 3
